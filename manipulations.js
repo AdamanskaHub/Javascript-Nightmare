a90f02da99a2d5e72edb620b36a2b5d3b7dcfa25
Emilie G <adamanska@gmail.com>
2017-06-26 12:08:23 +0200

    Down to round 2, fixed many issues, jshint crashes because of too many warnings

--- manipulations.js
+++ manipulations.js
@@ -1,63 +1,4 @@
 // ============================================
-// ============== CHARACTERS ==================
-// ============================================
-
-var Emilie = {
-    name: "Emilie",
-    happy: "img/emi-happy.png",
-    sad: "img/emi-sad.png",
-    grumpy: "img/emi-grumpy.png",
-    surprised: "img/emi-surprised.png",
-    sleeping: "img/emi-sleeping.png",
-    color: "#D893EE"
-};
-
-var JSMonster = {
-    name: "The Lord of Javascript",
-    neutral: "img/js-neutral.png",
-    threatning: "img/js-threatning.png",
-    color: "#93A4EE"
-};
-
-var Ajax = {
-    name: "Ajax",
-    neutral: "img/js-neutral.png",
-    threatning: "img/js-threatning.png",
-    color: "#93A4EE"
-};
-
-var Closure = {
-    name: "Closure",
-    neutral: "img/js-neutral.png",
-    threatning: "img/js-threatning.png",
-    color: "#93A4EE"
-};
-
-var RegEx = {
-    name: "RegEx",
-    neutral: "img/js-neutral.png",
-    threatning: "img/js-threatning.png",
-    color: "#93A4EE"
-};
-
-var mosquito = "img/mosquito.png";
-
-var BG = {
-    room: "img/bg-room.png",
-    hell: "img/bg-hell.jpg"
-
-};
-
-var sounds = {
-    sleep: "music/bg-room.png",
-    hell: "music/bg-hell.png",
-    round1: "",
-    round2: "",
-    round3: "",
-    final: ""
-};
-
-// ============================================
 // =========== DOC READY !!!!!! ===============
 // ============================================
 
@@ -65,8 +6,8 @@ $(document).ready(function() {
 
     var magicNum = 0;
 
-    var path_a = false;
-    var path_b = false;
+    var path_a, path_b, path_1a, path_1b, path_1c,
+        path_2a, path_2b, path_2c = false;
 
     $('button').on('click', function() {
 
@@ -74,6 +15,8 @@ $(document).ready(function() {
         console.log(magicNum);
 
         var js = nameChange(JSMonster.name, JSMonster.color);
+        var aj = nameChange(Ajax.name, Ajax.color);
+        var cl = nameChange(Closure.name, Closure.color);
         var txt = textChange("text", "");
         var emi = nameChange(Emilie.name, Emilie.color);
 
@@ -92,13 +35,16 @@ $(document).ready(function() {
                 txt;
                 break;
             case 4:
+                bgChange(BG.hell);
                 $("#char2").fadeOut();
+                $(".main").effect("shake");
                 emi;
                 bgChange(BG.hell);
                 char1Change(Emilie.grumpy);
                 // char2Change("");
                 break;
             case 5:
+                bgChange(BG.hell);
                 char1Change(Emilie.surprised);
                 emi;
                 break;
@@ -111,8 +57,10 @@ $(document).ready(function() {
             case 7:
                 nameChange(JSMonster.name, JSMonster.color);
                 txt;
+                $("p").css("font-size", "3em");
                 break;
             case 9:
+                $("p").css("font-size", "1.25em");
                 nameChange(JSMonster.name, JSMonster.color);
                 txt;
                 break;
@@ -125,11 +73,13 @@ $(document).ready(function() {
                 txt;
                 break;
             case 15:
+                $("p").css("font-size", "2em");
                 $("#char2").removeClass("border");
                 txt;
                 nameChange(JSMonster.name, JSMonster.color);
                 break;
             case 16:
+                $("p").css("font-size", "1.25em");
                 txt;
                 nameChange(JSMonster.name, JSMonster.color);
                 addButtons(
@@ -171,9 +121,218 @@ $(document).ready(function() {
                 txt;
                 break;
             case 21:
+                nameChange(JSMonster.name, JSMonster.color);
+                txt;
+                break;
+            case 23:
+                nameChange(JSMonster.name, JSMonster.color);
+                txt;
+                break;
+            case 25:
+                nameChange(JSMonster.name, JSMonster.color);
+                txt;
+                $("p").css("font-size", "1.5em");
+                break;
+            case 26:
+                $("p").css("font-size", "2em");
                 js;
                 txt;
                 break;
+            case 27:
+                $("p").css("font-size", "3em");
+                js;
+                txt;
+                break;
+            case 28:
+                $("p").css("font-size", "2em");
+                txt;
+                break;
+            case 29:
+                $("p").css("font-size", "2em");
+                nameChange(JSMonster.name, JSMonster.color);
+                txt;
+                break;
+            case 30:
+                $("p").css("font-size", "1.25em");
+                txt;
+                break;
+            case 32:
+                nameChange(JSMonster.name, JSMonster.color);
+                txt;
+                break;
+            case 33:
+
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 35:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 37:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 39:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 42:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 44:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                addThreeButtons();
+                $("#next").hide();
+                $('#button1').on('click', function() {
+                    { path_1a = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                $('#button2').on('click', function() {
+                    { path_1b = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                $('#button3').on('click', function() {
+                    { path_1c = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                break;
+
+            case 45:
+                nameChange(Ajax.name, Ajax.color);
+                if (path_1a) {
+                    textChange("text", "a");
+                } else if (path_1b) {
+                    textChange("text", "b");
+                } else if (path_1c) {
+                    textChange("text", "c");
+                }
+                break;
+            case 46:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 47:
+                aj;
+                txt;
+                break;
+            case 49:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                addThreeButtons2();
+                $("#next").hide();
+                $('#button1').on('click', function() {
+                    { path_2a = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                $('#button2').on('click', function() {
+                    { path_2b = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                $('#button3').on('click', function() {
+                    { path_2c = true; }
+                    deleteThreeButtons();
+                    $("#next").show();
+                });
+                break;
+
+            case 50:
+                nameChange(Ajax.name, Ajax.color);
+                if (path_2a) {
+                    textChange("text", "a");
+                } else if (path_2b) {
+                    textChange("text", "b");
+                } else if (path_2c) {
+                    textChange("text", "c");
+                }
+                break;
+            case 51:
+                $("p").css("font-size", "2.5em");
+                $(".char1").effect("shake");
+                txt;
+                break;
+            case 52:
+                $("p").css("font-size", "1.25em");
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 54:
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 55:
+                $("p").css("font-size", "2em");
+                txt;
+                break;
+            case 56:
+                $("p").css("font-size", "1.25em");
+                nameChange(Ajax.name, Ajax.color);
+                txt;
+                break;
+            case 57:
+                nameChange(Voice.name, Voice.color);
+                txt;
+                break;
+            case 58:
+                txt;
+                addButtons("Slide into oblivion!",
+                    "Fade the hell out of him");
+                $("#next").hide();
+                $('#button1').on('click', function() {
+                    { path_3a = true; }
+                    deleteButtons();
+                    $("#next").show();
+                });
+                $('#button2').on('click', function() {
+                    { path_3b = true; }
+                    deleteButtons();
+                    $("#next").show();
+                });
+                break;
+            case 59:
+                nameChange(Ajax.name, Ajax.color);
+                if (path_3a) {
+                    $('.char2').animate({ marginRight: "-1000px" }, 1500);
+                    textChange("text", "a");
+                } else if (path_3b) {
+                    textChange("text", "b");
+                    $('.char2').fadeOut("slow");
+                }
+                break;
+            case 61:
+                nameChange(Closure.name, Closure.color);
+                txt;
+                break;
+            case 62:
+                nameChange(Closure.name, Closure.color);
+                txt;
+                break;
+            case 65:
+                nameChange(Closure.name, Closure.color);
+                txt;
+                break;
+            case 66:
+                cl;
+                txt;
+                break;
+            case 68:
+                nameChange(Closure.name, Closure.color);
+                txt;
+                break;
+
+
+
+
+
+
+
             default:
                 emi;
                 textChange("text", "");
@@ -221,9 +380,33 @@ $(document).ready(function() {
         $(".button-space").append(b2);
     }
 
+    function addThreeButtons() {
+        var b1 = $("<button id='button1'>Rock</button>");
+        $(".button-space").append(b1);
+        var b2 = $("<button id='button2'>Paper</button>");
+        $(".button-space").append(b2);
+        var b3 = $("<button id='button3'>Scissors</button>");
+        $(".button-space").append(b3);
+    }
+
+    function addThreeButtons2() {
+        var b1 = $("<button id='button1'>Smash some scissors</button>");
+        $(".button-space").append(b1);
+        var b2 = $("<button id='button2'>Wrap some rocks</button>");
+        $(".button-space").append(b2);
+        var b3 = $("<button id='button3'>Cut some paper</button>");
+        $(".button-space").append(b3);
+    }
+
     function deleteButtons() {
         $("#button1").remove();
         $("#button2").remove();
+    }
+
+    function deleteThreeButtons() {
+        $("#button1").remove();
+        $("#button2").remove();
+        $("#button3").remove();
     }
 
     function nextOff() {
--- theText.js
+++ theText.js
@@ -1,3 +1,71 @@
+// ============================================
+// ============== CHARACTERS ==================
+// ============================================
+
+var Emilie = {
+    name: "Emilie",
+    happy: "img/emi-happy.png",
+    sad: "img/emi-sad.png",
+    grumpy: "img/emi-grumpy.png",
+    surprised: "img/emi-surprised.png",
+    sleeping: "img/emi-sleeping.png",
+    color: "#D893EE"
+};
+
+var Voice = {
+    name: "Distant voice",
+    color: "#93A4EE"
+};
+
+var JSMonster = {
+    name: "The Lord of Javascript",
+    neutral: "img/js-neutral.png",
+    threatning: "img/js-threatning.png",
+    color: "#93A4EE"
+};
+
+var Ajax = {
+    name: "Ajax",
+    neutral: "img/js-neutral.png",
+    threatning: "img/js-threatning.png",
+    color: "#93A4EE"
+};
+
+var Closure = {
+    name: "Closure",
+    neutral: "img/js-neutral.png",
+    threatning: "img/js-threatning.png",
+    color: "#93A4EE"
+};
+
+var RegEx = {
+    name: "RegEx",
+    neutral: "img/js-neutral.png",
+    threatning: "img/js-threatning.png",
+    color: "#93A4EE"
+};
+
+var mosquito = "img/mosquito.png";
+
+var BG = {
+    room: "img/bg-room.png",
+    hell: "img/bg-hell.jpg"
+
+};
+
+var sounds = {
+    sleep: "music/bg-room.png",
+    hell: "music/bg-hell.png",
+    round1: "",
+    round2: "",
+    round3: "",
+    final: ""
+};
+
+// ============================================
+// ================= TEXT =====================
+// ============================================
+
 var text_1 = "Bzzzz";
 var text_2 = "Go forth my minion and bring back that unworthy soul.";
 var text_3 = "Bzzzz zz z bzz bz zzzz ? Bzzzzz zzz bzz'z!";
@@ -21,20 +89,67 @@ var text_b17 = "You should not take it so lightly... Heed my warning.";
 // ========== CHOICE ======== //
 var text_18 = "This is your last chance to right your wrongs...";
 var text_19 = "I did nothing wrong.";
+
 var text_20 = "Enough! Can't you just focus and listen for two minutes?!";
 var text_21 = "This is probably why you can't get your head around function constructor.";
 var text_22 = "That's mean...";
-var text_23 = "Look";
-var text_24 = "Look";
-var text_25 = "Look";
-var text_26 = "Look";
-var text_27 = "Look";
-var text_28 = "Look";
-var text_29 = "Look";
-var text_30 = "Look";
-var text_31 = "Look";
-var text_32 = "Look";
-var text_33 = "Look";
-
-
-var text_b6 = "The text b2";
+var text_23 = "Follow the path to redemption. You will have to face three trials. Succeed in all of them and I will release you. Fail and you will be stuck here forever.";
+var text_24 = "It's not *that* bad here you know.";
+var text_25 = "And you will spend your days debugging...";
+var text_26 = "complex...";
+var text_27 = "...VANILLA Javascript.";
+var text_28 = "OH NO !!!!! Can't I die instead ? Can we talk about alternatives ? I have money ! ";
+var text_29 = "BWWHAAAAAAHAAAAAAHAAAAAAHAAAAAAAA";
+var text_30 = "Ok ok... Let's not succomb to panic.";
+var text_31 = "There must be a way out.";
+var text_32 = "Here is your first trial !";
+var text_33 = "I am Ajax, Lord of client-side...";
+var text_34 = "Stop right there. I haven't studied Ajax yet so this is just *not* fair.";
+var text_35 = "Don't interrupt me pitiful mortal!";
+var text_36 = "I will explain the rules of the trial.";
+var text_37 = "I will choose either rock, paper or scissors and...";
+var text_38 = "We're playing rock paper scissors?!";
+var text_39 = "What is it now mortal?!";
+var text_40 = "This has nothing to do with Javascript.";
+var text_41 = "And it's just luck! This is not fair.";
+var text_42 = "Would you prefer some Javascript riddle?";
+var text_43 = "Errr, no.";
+var text_44 = "Choose your answer.";
+// ========== CHOICE ========
+var text_45 = "KOOKOOKOKOKO";
+var text_a45 = "You chose 'Rock'. That is a shame, I chose 'PAPER'!";
+var text_b45 = "So you selected 'Paper'? My SCISSORS will tear you apart!";
+var text_c45 = "You think you can win with 'Scissors'? Not against my ROCK!";
+// ========== CHOICE ======== //
+var text_46 = "Hinhinhin! Fool!";
+var text_47 = "I've still got a chance.";
+var text_48 = "If I win again it's over for you.";
+var text_49 = "Make your choice.";
+// ========== CHOICE ========
+var text_50 = "Ca ne marche pas o__o";
+var text_a50 = "Too bad...I chose PAPER!";
+var text_b50 = "How unfortunate...I chose SCISSORS!";
+var text_c50 = "Not your lucky day...ROCK TIME!";
+// ========== CHOICE ======== //
+var text_51 = "NOOOOOOOOOOOOOOOOO!";
+var text_52 = "You thought you could beat me?! I am the Great Ajax, I *cannot* be beaten!";
+var text_53 = "Wait a minute... Do you mean that you cheated and I have no chance to win whatsoever?";
+var text_54 = "It is not 'cheating', I simply know your answers and can adapt.";
+var text_55 = "C H E A T I N G !";
+var text_56 = "I did not cheat!";
+var text_57 = "You know his Id, use it against him.";
+var text_58 = "His Id ? Of course !";
+// ========== CHOICE ========
+var text_59 = "Ca ne marche pas o__o";
+var text_a59 = "No! Don't!";
+var text_b59 = "Stop it!";
+// ========== CHOICE ======== //
+var text_60 = "Alright, trial number one cleared";
+var text_61 = "Well done.";
+var text_62 = "Let me introduce myself. I am Closure.";
+var text_63 = "Well, that was less pompous";
+var text_64 = "What's the challenge, let's do it.";
+var text_65 = "I am glad to see you so eager...";
+var text_66 = "...To play THE MUSIC QUIZ !";
+var text_67 = "A music quiz ? Now that's cool.";
+var text_68 = "Let us start the challenge.";
